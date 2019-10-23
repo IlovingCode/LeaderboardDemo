9f7e05ba20b31a84da27105dc40ff3754922ff7b
IlovingCode <ncthien93@gmail.com>
2019-10-23 21:11:25 +0700

    - update tracking

--- MrGun/assets/GAPlugin.js
+++ MrGun/assets/GAPlugin.js
@@ -17,7 +17,9 @@ cc.Class({
         ga.GameAnalytics.setEnabledVerboseLog(true);
 
         //ga.GameAnalytics.setEnabledEventSubmission(false);
-        //ga.GameAnalytics.configureAvailableResourceCurrencies(['coin']);
+        ga.GameAnalytics.configureAvailableResourceCurrencies(['coin']);
+        ga.GameAnalytics.configureAvailableResourceItemTypes(['kill', 'item']);
+        ga.GameAnalytics.configureAvailableCustomDimensions01(['headshot', 'armor']);
 
         ga.GameAnalytics.configureBuild(this.version);
         ga.GameAnalytics.initialize(this.key, this.secret);
@@ -25,35 +27,41 @@ cc.Class({
 
         gameEvent.GAME_START.push(this.onGameStart.bind(this));
         gameEvent.GAME_OVER.push(this.onGameOver.bind(this));
-        gameEvent.BOSS_HEALTH.push(this.onBossHealth.bind(this));
         gameEvent.COIN_CHANGED.push(this.onCoinChanged.bind(this));
 
-        ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Start, "session");
+        ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Complete, 'game', 'launch');
         this.playTime = 0;
+        this.coin = 0;
+
+        this.game = cc.find('Canvas').getComponent('GameController');
+        this.spawner = cc.find('Camera').getComponent('spawner');
     },
 
     onGameStart() {
-        ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Start, "startGame");
         this.playTime++;
+        ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Complete,
+            'game', 'session', 'start' + this.playTime);
+
+        let coin = this.game.coin - this.coin;
+        if (coin > 0) {
+            ga.GameAnalytics.addResourceEvent(ga.EGAResourceFlowType.Source,
+                'coin', coin, 'kill', 'headshot');
+            this.coin = this.game.coin;
+        }
     },
 
     onGameOver() {
-        let gameController = cc.find('Canvas').getComponent('GameController');
         ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Complete,
-            "startGame", gameController.score);
-    },
-
-    onBossHealth(amount) {
-        //ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Complete,
-        //    "startGame", gameController.score);
+            'game', 'session', 'end' + this.playTime);
+        ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Complete,
+            'game', 'boss_' + this.game.boss, 'enemy_' + (this.spawner.id % 9), this.game.score);
     },
 
     onCoinChanged(amount) {
+        if (amount > 0) return;
+        ga.GameAnalytics.addResourceEvent(ga.EGAResourceFlowType.Sink,
+            'coin', -amount, 'item', 'armor');
 
+        this.coin -= amount;
     },
-
-    onDestroy() {
-        ga.GameAnalytics.addProgressionEvent(ga.EGAProgressionStatus.Complete, 
-            "session", this.playTime);
-    }
 });
