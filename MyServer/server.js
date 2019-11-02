7bc651fdb41e11f7602623593a71b92d8af5d668
IlovingCode <ncthien93@gmail.com>
2019-11-02 20:34:33 +0700

    - update

--- MyServer/server.js
+++ MyServer/server.js
@@ -6,6 +6,7 @@ const app = express()
 
 const fs = require('fs');
 const database = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+const leaderboard = [];
 
 app.get('/mygame/hello', (req, res) => {
     res.send('Hi');
@@ -16,11 +17,48 @@ app.get('/mygame/user/:id', (req, res) => {
 });
 
 app.get('/mygame/user/:id/:score', (req, res) => {
-    let score = req.params.score;
-    let user = getUser(req.params.id);
-    if (+user.score < +score) user.score = score;
+    let score = +req.params.score;
+    let id = req.params.id;
+    let user = getUser(id);
+
+    let userScore = +user.score;
+    if (userScore < score) {
+        let arr = getLeaderboardItem(userScore);
+        arr.splice(arr.indexOf(id), 1);
+
+        arr = getLeaderboardItem(score);
+        arr.push(id);
+        user.score = score;
+
+        updateLeaderboard(score, userScore);
+    }
     res.send(JSON.stringify(user));
 });
+
+let getLeaderboardItem = function (id) {
+    let item = leaderboard[id];
+
+    if (!item) {
+        item = { start: 0, data: [] };
+        let length = leaderboard.length;
+        for (let i = id + 1; i < length; i++) {
+            let e = leaderboard[i];
+            if (e) {
+                item.start = e.start + e.data.length;
+                break;
+            }
+        }
+    }
+
+    return item;
+}
+
+let updateLeaderboard = function (id1, id2) {
+    for (let i = id2; i < id1; i++) {
+        let e = leaderboard[i];
+        if (e) e.start++;
+    }
+}
 
 let getUser = function (id) {
     if (!database[id]) {
