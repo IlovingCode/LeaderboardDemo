0484117db9593afe37c18a1ddcd675c9e2edf022
IlovingCode <ncthien93@gmail.com>
2019-11-25 01:34:23 +0700

    - add more affect

--- KnifeHit/assets/GameController.js
+++ KnifeHit/assets/GameController.js
@@ -10,24 +10,21 @@ cc.Class({
         content: cc.Node,
         score: cc.Label,
         stage: cc.Label,
+        preBoss: cc.Animation,
+        postBoss: cc.Animation,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
         gameEvent.resetAll();
-    },
-
-    start() {
-        cc.find('Camera').on('touchstart', gameEvent.invoke.bind(gameEvent, 'TAP'));
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, gameEvent.invoke.bind(gameEvent, 'TAP'), this);
         gameEvent.HIT.push(this.onHit.bind(this));
         gameEvent.GAME_OVER.push(this.onGameOver.bind(this));
 
-        let content = this.content;
-        let template = content.children[0];
         let isBoss = ++stage % 5 == 0;
 
+        let content = this.content;
+        let template = content.children[0];
         let count = this.count = (isBoss ? 13 : 7) + Math.floor(Math.random() * 3);
         while (--count > 0) {
             let obj = cc.instantiate(template);
@@ -55,6 +52,30 @@ cc.Class({
             isBoss ? cc.tintTo(0.1, 255, 100, 100) : cc.tintTo(0.1, 255, 200, 0),
             isBoss && cc.moveTo(0.3, 0, list[m].y),
         ));
+
+        let preBoss = this.preBoss;
+
+        if (isBoss) {
+            content.active = false;
+            this.enabled = false;
+            preBoss.node.active = true;
+            preBoss.once('finished', this.onFinish.bind(this));
+        } else {
+            preBoss.node.active = false;
+        }
+    },
+
+    onFinish() {
+        this.enabled = true;
+    },
+
+    start() {
+        cc.find('Camera').on('touchstart', gameEvent.invoke.bind(gameEvent, 'TAP'));
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, gameEvent.invoke.bind(gameEvent, 'TAP'), this);
+
+        //this.stage.enabled = true;
+        this.content.active = true;
+        gameEvent.invoke('START');
     },
 
     onHit() {
@@ -65,17 +86,27 @@ cc.Class({
 
         if (!count) {
             let isBoss = stage % 5 == 0;
+            let stageObj = this.stage;
+            stageObj.enabled = false;
             if (isBoss) {
-                let stageObj = this.stage;
                 let list = stageObj.node.children;
                 let m = list.length - 1;
 
                 list[m].runAction(cc.sequence(
                     cc.tintTo(0.1, 255, 255, 255),
-                    cc.moveTo(0.3, 106, list[m].y),
+                    cc.moveTo(0.3, 106, list[m].y)
+                ));
+
+                let preBoss = this.preBoss;
+                preBoss.once('finished', cc.director.loadScene.bind(cc.director, 'game', null, null));
+                preBoss.play('postBoss');
+            } else {
+                stageObj.node.runAction(cc.sequence(
+                    cc.delayTime(0.4),
                     cc.callFunc(cc.director.loadScene.bind(cc.director, 'game', null, null))
                 ));
-            } else cc.director.loadScene('game');
+            }
+            gameEvent.invoke('STAGE');
         } else gameEvent.invoke('KNIFE');
     },
 
--- KnifeHit/assets/Knife.js
+++ KnifeHit/assets/Knife.js
@@ -4,6 +4,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        collisionAngle: 9,
         sprites: [cc.SpriteFrame],
     },
 
@@ -12,18 +13,29 @@ cc.Class({
     // onLoad () {},
 
     start() {
-        let node = this.node;
+        this.enabled = false;
         let knife = this.knife = cc.find('knife');
-        this.y = knife.y;
+        knife.active = false;
+
+        gameEvent.START.push(this.onGameStart.bind(this));
+    },
+
+    onGameStart() {
+        let node = this.node;
+        let knife = this.knife;
+        knife.active = true;
+        this.enabled = true;
+        let y = this.y = knife.y;
         this.seq1 = cc.sequence(cc.moveTo(0.1, 0, node.y - 350), cc.callFunc(this.onHit.bind(this)));
         this.seq2 = cc.sequence(cc.moveBy(0.1, 0, 30), cc.moveBy(0.1, 0, -30));
         this.seq3 = cc.sequence(cc.fadeTo(0.1, 200), cc.fadeOut(0.1));
         this.seq4 = cc.sequence(cc.moveTo(0.1, 0, this.y), cc.callFunc(this.onResume.bind(this)));
-        this.seq5 = cc.sequence(cc.spawn(cc.rotateBy(1, 1300), cc.jumpTo(1, 0, this.y - 2000, 0, 1))
+        this.seq5 = cc.sequence(cc.spawn(cc.rotateBy(1, 1300), cc.jumpTo(1, 0, y - 2000, 0, 1))
             , cc.callFunc(this.onFailed.bind(this)));
 
         gameEvent.TAP.push(this.onclick.bind(this));
         gameEvent.KNIFE.push(this.onKnife.bind(this));
+        gameEvent.STAGE.push(this.onStage.bind(this));
 
         let sprite = node.children[0];
         let id = Math.floor(Math.random() * this.sprites.length);
@@ -33,12 +45,13 @@ cc.Class({
         this.cover.setContentSize(sprite);
 
         this.rotList = [];
-        knife.setPosition(0, this.y - 200);
+        knife.setPosition(0, y - 200);
         knife.runAction(this.seq4);
 
         this.pause = true;
         sprite.scale = 0;
         sprite.runAction(cc.sequence(cc.scaleTo(0.2, 1.15), cc.scaleTo(0.1, 1)));
+        node.getComponent('Target').enabled = true;
     },
 
     onFailed() {
@@ -54,8 +67,9 @@ cc.Class({
 
         while (rotation < 0) rotation += 360;
         rotation = Math.floor(rotation) % 360;
+        let angle = this.collisionAngle;
         for (let r of list) {
-            if (Math.abs(r - rotation) < 9) {
+            if (Math.abs(r - rotation) < angle) {
                 knife.runAction(this.seq5);
                 return;
             }
@@ -72,6 +86,10 @@ cc.Class({
         this.cover.stopAllActions();
         node.runAction(this.seq2);
         this.cover.runAction(this.seq3);
+    },
+
+    onStage() {
+        this.node.runAction(cc.sequence(cc.scaleTo(0.15, 1.2), cc.scaleTo(0.1, 0)));
     },
 
     onKnife() {
