a44c7d6a05b5fff57e8c98de72edb8eb3b29167b
Dante <nttnghia20@gmail.com>
2020-01-09 19:16:22 +0700

    * fix bug merge coin and score for htx

--- KnifeHit/assets/Profile.js
+++ KnifeHit/assets/Profile.js
@@ -1,8 +1,8 @@
 var Profile = module.exports = {};
 
 Profile.load = function () {
-    this.bestScore = cc.sys.localStorage.getItem('bestScore') || 0;
-    this.coin = cc.sys.localStorage.getItem('coin') || 0;
+    this.bestScore = cc.sys.localStorage.getItem('htx_bestScore') || 0;
+    this.coin = cc.sys.localStorage.getItem('htx_coin') || 0;
     cc.log('Load Profile');
     Profile.load = function () { };
 }
@@ -10,9 +10,9 @@ Profile.load = function () {
 Profile.set = function (score, coin) {
     if (this.bestScore < score) {
         this.bestScore = score;
-        cc.sys.localStorage.setItem('bestScore', score);
+        cc.sys.localStorage.setItem('htx_bestScore', score);
     }
 
     this.coin = coin;
-    cc.sys.localStorage.setItem('coin', coin);
+    cc.sys.localStorage.setItem('htx_coin', coin);
 }
